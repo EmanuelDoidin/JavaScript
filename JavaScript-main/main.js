bad9826569b9a050917c0bcc32bbabb008ca31ad
EmanuelDoidin <129960275+EmanuelDoidin@users.noreply.github.com>
2023-11-01 10:49:09 -0300

    Add files via upload

---
+++ JavaScript-main/main.js
@@ -0,0 +1,31 @@
+function tocaSomPom(){
+const elemneto = document.querySelector('#som_tecla_pom').play('')
+}
+
+
+if(elemento != null && elemento.localName === 'audio'){
+    elemento.play();
+}else{
+    alert('Elemento não encontrado');
+}
+const lista = document.querySelectorAll('.tecla');
+
+for(contador = 0; contador < lista.length;){}
+
+while(contador < 9, lista.length){
+    lista [contador].classList[1];
+
+
+    const instrumento = lista[contador];
+    const tecla = lista[contador];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla[contador].onclick = function (){
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento){
+        if(evento.code=='Space' || evento.code ==='Enter'){
+            tecla.classList.add('ativa');}
+    }
+    }
